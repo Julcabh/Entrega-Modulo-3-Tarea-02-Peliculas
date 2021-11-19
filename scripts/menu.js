c9ba99ed4955170db07c5a2dd9edeea4bb5992bb
Julian Caballero <julian2895@gmail.com>
2021-11-19 14:24:34 -0500

    entrega 02

---
+++ scripts/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById("btnMenu");
+var nav = document.getElementById("btnMenu");
+
+btnMenu.addEventListener("click", function(){
+    nav.classList.toggle("muestrate")
+})
